f0e508370cb649029236ffffa6006310699c1e88
Amit Atsmon <amitatsmon1@gmail.com>
2016-12-31 12:24:05 +0200

    added documentation to main_aux.h

--- ex2/main_aux.h
+++ ex2/main_aux.h
@@ -1,4 +1,13 @@
 #ifndef MAIN_AUX_H
 #define MAIN_AUX_H
+
+/**
+ * The function reads doubles from the user and stores them in a given array
+ *
+ * @param in_data - A pointer to where to put the input array
+ * @param dim - The number of doubles to read into in_data
+ * 
+ */
 void get_point_input(double* in_data, int dim);
+
 #endif	
